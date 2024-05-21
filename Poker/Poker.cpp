f8c1f9338a0248f56b6051a51e50a3915145c3dc
szymon <szymon@MUREJOWYKOMP>
2024-05-21 22:15:28 +0200

    dodane menu

--- Poker/Poker.cpp
+++ Poker/Poker.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 int main()
 {
-    cout << "Hello World!" << endl;
-    cout << "Jebać disa!";
-    cout << "Kocham disa";
-    cout << "Chuj";
+	cout << "Witaj w grze Poker!" << endl << "Wybierz opcję: " << endl << "1) Rozpocznij partię" << endl << "2) Wyjdź z gry";
+	int tytul_input = 0;
+	cin >> tytul_input;
+	if (tytul_input == 1) {
+		cout << "Tu będą opcje...";
+	}
+	else {
+		return 0;
+	}
+
 }
 
 
