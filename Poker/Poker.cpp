b2c4f79be1c9b05ca0bd9e974e9643fb8b09b1e6
Kubixon214 <jakubluzynski2004@gmail.com>
2024-05-23 23:15:52 +0200

    Narazie zrobiłem tyle jescze ich nie połączyłem

--- Poker/Poker.cpp
+++ Poker/Poker.cpp
@@ -1,23 +1,61 @@
 #include <iostream>
 using namespace std;
-class Gra{
+class Gracz {
 public:
-
-
-private:
-
-};
-class Bot{
+	int id;
+	int kapital;
+	string karty;
+	int stawka;
+//	boolean dealer;
+	string status;
+	Gracz(int id, int kapital, string karty, int stawka) {
+		this->id = id;
+		this->kapital = kapital;
+		this->karty = karty;
+		this->stawka=stawka;
+	}
+	void Ruch_Czlowieka();
 };
 class Czlowiek
 {
 public:
+	Czlowiek(int id, int kapital, string karty, int stawka, string status) {
+		this->id = id;
+		this->kapital = kapital;
+		this->karty = karty;
+		this->stawka = stawka;
+		this->status = status;
+	}
+};
+class Bot {
+private:
+	int  poziom_trudnosci;
+public:
+	Bot(int id, int kapital, string karty, int stawka, string status,int poziom_trudnosci)
+	this->id = id;
+	this->kapital = kapital;
+	this->karty = karty;
+	this->stawka = stawka;
+	this->status = status;
+	this->poziom_trudnosci = poziom_trudnosci;
+};
+class Gra{
+protected:
+	string talia;
+public:
+	string stol;
+	int pula;
+	void RozdajKarty();
+	void TworzGraczy();
+	int SetDealer();
+	void RozpocznijRunde();
+	void OdkryjStol();
+	void WinnerFinder();
 
 private:
+	int liczba_graczy;
+};
 
-};
-class Gracz {
-};
 int main()
 {
 	cout << "Witaj w grze Poker!" << endl << "Wybierz opcję: " << endl << "1) Rozpocznij partię" << endl << "2) Wyjdź z gry";
