6a6ba173d2ca1f9bf037fc1a098d42f41da848ee
szymon <szymon@MUREJOWYKOMP>
2024-05-24 23:32:52 +0200

    Patrze czy działa

--- Poker/Poker.cpp
+++ Poker/Poker.cpp
@@ -1,59 +1,61 @@
 #include <iostream>
 using namespace std;
+
 class Gracz {
-public:
-	int id;
-	int kapital;
-	string karty;
-	int stawka;
-//	boolean dealer;
-	string status;
-	Gracz(int id, int kapital, string karty, int stawka) {
-		this->id = id;
-		this->kapital = kapital;
-		this->karty = karty;
-		this->stawka=stawka;
-	}
-	void Ruch_Czlowieka();
+	public:
+		int id;
+		int kapital;
+		string karty;
+		int stawka;
+	//	boolean dealer;
+		string status;
+		Gracz(int id, int kapital, string karty, int stawka, string status) {
+			this->id = id;
+			this->kapital = kapital;
+			this->karty = karty;
+			this->stawka=stawka;
+		}
+		//void Ruch_Czlowieka();
 };
-class Czlowiek
-{
-public:
-	Czlowiek(int id, int kapital, string karty, int stawka, string status) {
-		this->id = id;
-		this->kapital = kapital;
-		this->karty = karty;
-		this->stawka = stawka;
-		this->status = status;
-	}
+
+class Czlowiek : public Gracz{
+	public:
+		Czlowiek(int id, int kapital, string karty, int stawka, string status){
+			this->id = id;
+			this->kapital = kapital;
+			this->karty = karty;
+			this->stawka = stawka;
+			this->status = status;
+		}
 };
-class Bot {
-private:
-	int  poziom_trudnosci;
-public:
-	Bot(int id, int kapital, string karty, int stawka, string status,int poziom_trudnosci)
-	this->id = id;
-	this->kapital = kapital;
-	this->karty = karty;
-	this->stawka = stawka;
-	this->status = status;
-	this->poziom_trudnosci = poziom_trudnosci;
+
+class Bot : public Gracz{
+	private:
+		int  poziom_trudnosci;
+	public:
+		Bot(int id, int kapital, string karty, int stawka, string status, int poziom_trudnosci){
+			this->id = id;
+			this->kapital = kapital;
+			this->karty = karty;
+			this->stawka = stawka;
+			this->status = status;
+			this->poziom_trudnosci = poziom_trudnosci;
+		}
 };
 class Gra{
-protected:
-	string talia;
-public:
-	string stol;
-	int pula;
-	void RozdajKarty();
-	void TworzGraczy();
-	int SetDealer();
-	void RozpocznijRunde();
-	void OdkryjStol();
-	void WinnerFinder();
-
-private:
-	int liczba_graczy;
+	protected:
+		string talia;
+	public:
+		string stol;
+		int pula;
+		void RozdajKarty();
+		void TworzGraczy();
+		int SetDealer();
+		void RozpocznijRunde();
+		void OdkryjStol();
+		void WinnerFinder();
+	private:
+		int liczba_graczy;
 };
 
 int main()
@@ -70,4 +72,6 @@ int main()
 
 }
 
-
+void Gracz::Ruch_Czlowieka()
+{
+}
