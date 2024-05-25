3cee205416852df40264acee3d1a72bcdf61a286
szymon <szymon@MUREJOWYKOMP>
2024-05-25 19:42:15 +0200

    coś jeszcze dodałem

--- Poker/Poker.cpp
+++ Poker/Poker.cpp
@@ -33,7 +33,12 @@ class Bot : public Gracz{
 };
 class Gra{
 	protected:
-		string talia;
+		string talia[52] = {
+		"As Karo", "2 Karo", "3 Karo", "4 Karo", "5 Karo", "6 Karo", "7 Karo", "8 Karo", "9 Karo", "10 Karo", "Walet Karo", "Dama Karo", "Król Karo",
+		"As Kier", "2 Kier", "3 Kier", "4 Kier", "5 Kier", "6 Kier", "7 Kier", "8 Kier", "9 Kier", "10 Kier", "Walet Kier", "Dama Kier", "Król Kier",
+		"As Trefl", "2 Trefl", "3 Trefl", "4 Trefl", "5 Trefl", "6 Trefl", "7 Trefl", "8 Trefl", "9 Trefl", "10 Trefl", "Walet Trefl", "Dama Trefl", "Król Trefl",
+		"As Pik", "2 Pik", "3 Pik", "4 Pik", "5 Pik", "6 Pik", "7 Pik", "8 Pik", "9 Pik", "10 Pik", "Walet Pik", "Dama Pik", "Król Pik"
+		};
 	public:
 		string stol;
 		int pula;
@@ -90,6 +95,7 @@ int main()
 			cout << "Wprowadzono niepoprawną wartość! " << endl << "Podaj kapitał początkowy każdego gracza (min. 100): " << endl;
 			cin >> kapital;
 		}
+
 	}
 	else if(tytul_input == 2){
 		return 0;
