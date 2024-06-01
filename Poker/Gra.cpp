8145bcab2df88182d2cfd2a27b76537347ae19f7
Kubixon214 <jakubluzynski2004@gmail.com>
2024-06-01 21:00:36 +0200

    Moje porównywanie kart

--- Poker/Gra.cpp
+++ Poker/Gra.cpp
@@ -2,6 +2,11 @@
 
 Gra::Gra() {
     ResetTalia();
+    // Initialize card rank values
+    cardRankValues = {
+        {"2", 2}, {"3", 3}, {"4", 4}, {"5", 5}, {"6", 6}, {"7", 7}, {"8", 8}, {"9", 9},
+        {"10", 10}, {"Walet", 11}, {"Dama", 12}, {"KrÛl", 13}, {"As", 14}
+    };
 }
 
 void Gra::ResetTalia() {
@@ -9,6 +14,7 @@ void Gra::ResetTalia() {
     unsigned seed = chrono::system_clock::now().time_since_epoch().count();
     shuffle(talia.begin(), talia.end(), default_random_engine(seed));
 }
+
 void Gra::RozdajKarty(vector<Gracz*>& gracze) {
     ResetTalia();
     for (auto& gracz : gracze) {
@@ -173,25 +179,136 @@ bool Gra::SprawdzajStatus(){
 }
 
 void Gra::WinnerFinder() {
-    Gracz* zwyciezca = nullptr;
-    int max_kapital = -1;
+    Gracz* winner = nullptr;
+    PokerHandRank winnerRank = HIGH_CARD;
 
     for (auto& gracz : gracze) {
-        if (gracz->kapital > max_kapital) {
-            max_kapital = gracz->kapital;
-            zwyciezca = gracz;
+        // Combine player's cards with community cards (stol)
+        vector<string> allCards;
+        allCards.push_back(gracz->karty[0]);
+        allCards.push_back(gracz->karty[1]);
+        allCards.insert(allCards.end(), stol, stol + 5);
+
+        // Evaluate the poker hand
+        PokerHandRank rank = EvaluatePokerHand(allCards);
+
+        // Update the winner if the current player's hand outranks the previous winner
+        if (rank > winnerRank) {
+            winner = gracz;
+            winnerRank = rank;
         }
-        gracz->stawka = 0; // Reset each player's stake
     }
 
-    if (zwyciezca != nullptr) {
-        if (zwyciezca->id == 0) {
-            cout << "Zwyciezca rundy to: Czlowiek" << endl;
-        }
-        else {
-            cout << "Zwyciezca rundy to: Bot ID: " << zwyciezca->id << endl;
-        }
-        cout << "Kapital zwyciezcy: " << zwyciezca->kapital << endl;
+    // Print the winner
+    if (winner != nullptr) {
+        cout << "Zwyciezca rundy to: " << winner->id << endl;
+        cout << "Ranking pokerowy: " << winnerRank << endl;
     }
 }
+bool Gra::IsFlush(const vector<string>& cards) {
+    char suit = cards[0][cards[0].find_last_of(' ') + 1]; // Odczytaj kolor pierwszej karty
+
+    // Sprawdü, czy wszystkie pozosta≥e karty majπ ten sam kolor
+    for (int i = 1; i < cards.size(); ++i) {
+        if (cards[i][cards[i].find_last_of(' ') + 1] != suit) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Gra::IsStraight(const vector<string>& cards) {
+    // Konwertuj wartoúci kart na liczby
+    vector<int> cardValues;
+    for (const auto& card : cards) {
+        string value = card.substr(0, card.find(' '));
+        if (cardRankValues.find(value) != cardRankValues.end()) {
+            cardValues.push_back(cardRankValues[value]);
+        }
+    }
+
+    // Posortuj karty
+    sort(cardValues.begin(), cardValues.end());
+
+    // Sprawdü, czy karty tworzπ ciπg wartoúci
+    for (int i = 1; i < cardValues.size(); ++i) {
+        if (cardValues[i] != cardValues[i - 1] + 1) {
+            return false;
+        }
+    }
+    return true;
+}
+
+Gra::PokerHandRank Gra::EvaluatePokerHand(const vector<string>& cards) {
+    // Sprawdü, czy wszystkie karty majπ ten sam kolor
+    bool flush = IsFlush(cards);
+
+    // Sprawdü, czy karty tworzπ strita
+    bool straight = IsStraight(cards);
+
+    // Wartoúci kart
+    vector<int> cardValues;
+    for (const auto& card : cards) {
+        string value = card.substr(0, card.find(' '));
+        if (cardRankValues.find(value) != cardRankValues.end()) {
+            cardValues.push_back(cardRankValues[value]);
+        }
+    }
+
+    // Posortuj wartoúci kart
+    sort(cardValues.begin(), cardValues.end());
+
+    // Sprawdü, czy wystÍpujπ pary, trÛjki itp.
+    map<int, int> valueCount;
+    for (int value : cardValues) {
+        valueCount[value]++;
+    }
+
+    int pairCount = 0;
+    int threeOfAKind = 0;
+    int fourOfAKind = 0;
+
+    for (const auto& pair : valueCount) {
+        if (pair.second == 2) {
+            pairCount++;
+        }
+        else if (pair.second == 3) {
+            threeOfAKind++;
+        }
+        else if (pair.second == 4) {
+            fourOfAKind++;
+        }
+    }
+
+    // Okreúl wynik na podstawie kombinacji kart
+    if (straight && flush) {
+        return STRAIGHT_FLUSH;
+    }
+    else if (fourOfAKind > 0) {
+        return FOUR_OF_A_KIND;
+    }
+    else if (threeOfAKind > 0 && pairCount > 0) {
+        return FULL_HOUSE;
+    }
+    else if (flush) {
+        return FLUSH;
+    }
+    else if (straight) {
+        return STRAIGHT;
+    }
+    else if (threeOfAKind > 0) {
+        return THREE_OF_A_KIND;
+    }
+    else if (pairCount >= 2) {
+        return TWO_PAIR;
+    }
+    else if (pairCount == 1) {
+        return ONE_PAIR;
+    }
+    else {
+        return HIGH_CARD;
+    }
+}
+
+
 
--- Poker/Gra.h
+++ Poker/Gra.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <map>
 #include <vector>
 #include <iostream>
 #include <random>   
@@ -7,36 +8,65 @@
 #include "Gracz.h"
 #include "Czlowiek.h"
 #include "Bot.h"
+
 using namespace std;
 
 int sprawdzam(int x);
 
 class Gra
 {
-	protected:
-	    vector<string> talia;
-	    const vector<string> initial_talia = {
-	        "As Karo", "2 Karo", "3 Karo", "4 Karo", "5 Karo", "6 Karo", "7 Karo", "8 Karo", "9 Karo", "10 Karo", "Walet Karo", "Dama Karo", "Król Karo",
-	        "As Kier", "2 Kier", "3 Kier", "4 Kier", "5 Kier", "6 Kier", "7 Kier", "8 Kier", "9 Kier", "10 Kier", "Walet Kier", "Dama Kier", "Król Kier",
-	        "As Trefl", "2 Trefl", "3 Trefl", "4 Trefl", "5 Trefl", "6 Trefl", "7 Trefl", "8 Trefl", "9 Trefl", "10 Trefl", "Walet Trefl", "Dama Trefl", "Król Trefl",
-	        "As Pik", "2 Pik", "3 Pik", "4 Pik", "5 Pik", "6 Pik", "7 Pik", "8 Pik", "9 Pik", "10 Pik", "Walet Pik", "Dama Pik", "Król Pik"
-	    };
-	public:
-	    string stol[5];
-	    int pula;
-	    void RozdajKarty(vector<Gracz*>& gracze);
-	    int SetDealer(int numer_rundy);
-	    void RozpocznijRunde();
-	    void OdkryjStol();
-	    void WinnerFinder();
-	    void ResetTalia();
-		void TworzGraczy(int liczba_graczy);
-		bool SprawdzajStatus();
-		int numer_rundy = 1;
-		int max_stawka = 0;
-		Gra();
-	private:
-		vector<Gracz*> gracze;
-		vector<Gracz*> tempGracze;
+protected:
+    vector<string> talia;
+    const vector<string> initial_talia = {
+        "As Karo", "2 Karo", "3 Karo", "4 Karo", "5 Karo", "6 Karo", "7 Karo", "8 Karo", "9 Karo", "10 Karo", "Walet Karo", "Dama Karo", "Król Karo",
+        "As Kier", "2 Kier", "3 Kier", "4 Kier", "5 Kier", "6 Kier", "7 Kier", "8 Kier", "9 Kier", "10 Kier", "Walet Kier", "Dama Kier", "Król Kier",
+        "As Trefl", "2 Trefl", "3 Trefl", "4 Trefl", "5 Trefl", "6 Trefl", "7 Trefl", "8 Trefl", "9 Trefl", "10 Trefl", "Walet Trefl", "Dama Trefl", "Król Trefl",
+        "As Pik", "2 Pik", "3 Pik", "4 Pik", "5 Pik", "6 Pik", "7 Pik", "8 Pik", "9 Pik", "10 Pik", "Walet Pik", "Dama Pik", "Król Pik"
+    };
+
+public:
+    string stol[5];
+    int pula;
+    void RozdajKarty(vector<Gracz*>& gracze);
+    int SetDealer(int numer_rundy);
+    void RozpocznijRunde();
+    void OdkryjStol();
+    void WinnerFinder();
+    void ResetTalia();
+    void TworzGraczy(int liczba_graczy);
+    bool SprawdzajStatus();
+    int numer_rundy = 1;
+    int max_stawka = 0;
+    Gra();
+
+private:
+    vector<Gracz*> gracze;
+    vector<Gracz*> tempGracze;
+
+    // Define ranks for poker hands
+    enum PokerHandRank {
+        HIGH_CARD,
+        ONE_PAIR,
+        TWO_PAIR,
+        THREE_OF_A_KIND,
+        STRAIGHT,
+        FLUSH,
+        FULL_HOUSE,
+        FOUR_OF_A_KIND,
+        STRAIGHT_FLUSH
+    };
+
+    // Define a map to convert card ranks to numeric values for comparison
+    map<string, int> cardRankValues;
+
+    // Define a function to check if all cards have the same suit
+    bool IsFlush(const vector<string>& cards);
+
+    // Define a function to check if cards form a straight
+    bool IsStraight(const vector<string>& cards);
+
+    // Define a function to evaluate the poker hand
+    PokerHandRank EvaluatePokerHand(const vector<string>& cards);
 };
+
 
